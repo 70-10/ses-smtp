ecbbabd9428a7e283651854766cb2ef5a8f691a9
70_10 <7010.dev@gmail.com>
2017-02-18 14:22:08 +0900

    [add] safe-buffer

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const crypto = require("crypto");
+const Buffer = require("safe-buffer").Buffer;
 
 module.exports = function (awsSecretKey) {
-  const VERSION = new Buffer([2]);
+  const VERSION = Buffer.from([2]);
   const MESSAGE = "SendRawEmail";
 
   const signature = crypto.createHmac("sha256", awsSecretKey).update(MESSAGE).digest();
